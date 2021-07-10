aa8b56ce36f9b27ee6208273410fde2395b23c46
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2021-07-10 16:01:01 -0300

    alterar tipo dos campos

--- av3/inquilino.sql
+++ av3/inquilino.sql
@@ -1,7 +1,7 @@
 CREATE TABLE inquilino (
     pessoa_cpf varchar2(11),
-    renda int,
-    valor_max_desejado int,
+    renda decimal(7,2),
+    valor_max_desejado decimal(7,2),
     CONSTRAINT inquilino_pkey PRIMARY KEY (pessoa_cpf),
     CONSTRAINT pessoa_cpf_fkey FOREIGN KEY (pessoa_cpf) REFERENCES pessoa (cpf)
 );
--- av3/parcela.sql
+++ av3/parcela.sql
@@ -2,7 +2,7 @@ CREATE TABLE parcela (
     numero_contrato integer,
     codigo integer,
     data_vencimento date,
-    valor decimal(7,2)
+    valor decimal(7,2),
     CONSTRAINT parcela_pkey PRIMARY KEY (numero_contrato, codigo),
     CONSTRAINT numero_contrato_fkey FOREIGN KEY (numero_contrato) REFERENCES contrato (numero),
     CONSTRAINT parcela_check CHECK (valor > 0.0)
