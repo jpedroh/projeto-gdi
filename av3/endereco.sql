aaadbb50d090dcb2a8971c3ddbe7f3f844eca068
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2021-07-10 16:01:10 -0300

    adicionar povoamento de endereco

--- av3/endereco.sql
+++ av3/endereco.sql
@@ -4,3 +4,14 @@ CREATE TABLE endereco (
     bairro varchar2(15),
     CONSTRAINT endereco_pkey PRIMARY KEY (cep)
 );
+
+INSERT INTO endereco VALUES ("50740445", "Rua Visconde de Barbacena", "Cidade Universitaria");
+INSERT INTO endereco VALUES ("51011330", "Rua João Bandeira de Melo", "Pina");
+INSERT INTO endereco VALUES ("50910510", "Rua Professor Pedro Augusto", "Jardim São Paulo");
+INSERT INTO endereco VALUES ("51010450", "Travessa Albacora", "Brasília Teimosa");
+INSERT INTO endereco VALUES ("50620491", "Rua Piranhas", "Torre");
+INSERT INTO endereco VALUES ("50790901", "Rua Leandro Barreto", "Jardim São Paulo");
+INSERT INTO endereco VALUES ("51011061", "Rua Vila Alegre", "Pina");
+INSERT INTO endereco VALUES ("51160350", "Rua José da Silva Lucena", "Imbiribeira");
+INSERT INTO endereco VALUES ("51345520", "Rua Bom Jardim", "COHAB");
+INSERT INTO endereco VALUES ("50630974", "Avenida General San Martin", "Cordeiro");
