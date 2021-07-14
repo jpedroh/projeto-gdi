174fd2877ea07bb3fbba758ecf66391e0b4ab335
Joao Pedro <joao.pedro.hsd@gmail.com>
2021-07-13 22:18:16 -0300

    morever arquivos velhos

--- av3/parcela.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE parcela (
-    numero_contrato integer,
-    codigo integer,
-    data_vencimento date,
-    valor decimal(7,2),
-    CONSTRAINT parcela_pkey PRIMARY KEY (numero_contrato, codigo),
-    CONSTRAINT parcela_fkey FOREIGN KEY (numero_contrato) REFERENCES contrato (numero),
-    CONSTRAINT parcela_check CHECK (valor > 0.0)
-);
